558259fdd1bf82599773e22479905d2178df5656
cjallen1988 <cjallen1988@gmail.com>
2022-12-08 11:58:34 -0600

    day 1 complete

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const run = require('./day1/problem1/problem1.js'); // correct answer is 66487
+// const run = require('./day1/problem1/problem1.js'); // correct answer is 66487
+const run = require('./day1/problem2/problem2.js'); // correct answer is 197301
 
 console.log(run());
 
--- day1/problem2/problem2.js
+++ day1/problem2/problem2.js
@@ -6,7 +6,7 @@ const fileLocation = './day1/data.txt';
 function mostSnacks(data) {
     let most = 0;
     data.sort(function(a, b) {return b-a}); // sort descending order
-    return data[0]; // grab highest according to sort
+    return data[0] + data[1] + data[2]; // grab highest 3 according to sort
 }
 
 function run() {
